02e2b58242ceebddfc95500c858f650354d5ef95
anaxmaria <ianamaria99@yahoo.com>
2021-08-24 23:51:14 +0300

    create quasar page

---
+++ qwitter/src/layouts/MainLayout.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold"> Qwitter </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer 
+      show-if-above 
+      v-model="leftDrawerOpen" 
+      side="left" 
+      :width="283"
+      bordered
+    >
+      <!-- drawer content -->
+      <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
+
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="fas fa-home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
